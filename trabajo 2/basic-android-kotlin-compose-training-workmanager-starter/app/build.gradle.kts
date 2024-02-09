3a8fff5e2b3f97b956efc77da83051ec954144a7
yahir <s20120196@alumnos.itsur.edu.mx>
2024-02-09 15:17:44 -0600

    Se agrego la dependencia

--- trabajo 2/basic-android-kotlin-compose-training-workmanager-starter/app/build.gradle.kts
+++ trabajo 2/basic-android-kotlin-compose-training-workmanager-starter/app/build.gradle.kts
@@ -66,7 +66,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.work:work-runtime-ktx:2.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.10.00"))
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.activity:activity-ktx:1.8.0")
