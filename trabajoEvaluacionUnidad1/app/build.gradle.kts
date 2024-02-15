ba73ed949a4d405d3a79c0b91cc42f5f39e68c49
yahir <s20120196@alumnos.itsur.edu.mx>
2024-02-14 20:04:30 -0600

    Se agregaron las dependencias y clases kotlin base para realizar la practica

--- trabajoEvaluacionUnidad1/app/build.gradle.kts
+++ trabajoEvaluacionUnidad1/app/build.gradle.kts
@@ -66,4 +66,10 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Retrofit for API requests
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    // ViewModel and LiveData for MVVM architecture
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
+    implementation ("androidx.lifecycle:lifecycle-livedata:2.3.1")
 }
