89af1e024b6aa884dd9e9e14686aeb232c369fca
yahir <s20120196@alumnos.itsur.edu.mx>
2024-03-05 20:17:25 -0600

    Se modificaron algunos request, tambien se agregaron los workers para trabajar en background aunque hay algunos detalles todavia, ademas  la base de datos ya esta casi terminada

--- UNIDAD2/trabajoEvaluacionUnidad2/evaluacion/modelos/CredencialesAlumno.kt
+++
@@ -1,9 +0,0 @@
-package com.example.autenticacionyconsulta.modelos
-
-data class CredencialesAlumno(
-   var acceso:String,
-   var estatus:String,
-   var tipoUsuario:Int,
-   var contrasenia:String,
-   var matricula:String
-)
--- UNIDAD2/trabajoEvaluacionUnidad2/evaluacion/navigation/AppScreens.kt
+++
@@ -1,8 +0,0 @@
-package com.example.autenticacionyconsulta.navigation
-
-sealed class AppScreens(val route:String) {
-    object Login:AppScreens("Login")
-    object Info:AppScreens("Info")
-    object Kardex:AppScreens("Kardex")
-    object Horario:AppScreens("Horario")
-}
