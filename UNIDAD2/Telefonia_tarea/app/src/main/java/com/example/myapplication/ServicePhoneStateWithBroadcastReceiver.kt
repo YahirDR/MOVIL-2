312419fd25f0b2d0566b8f44dbcfab038fb6c074
yahir <s20120196@alumnos.itsur.edu.mx>
2024-03-09 22:30:52 -0600

    Se comenzo con la practica telefonia, ademas de la base de datos de la evaluacion unidad 2, ya guarda datos

---
+++ UNIDAD2/Telefonia_tarea/app/src/main/java/com/example/myapplication/ServicePhoneStateWithBroadcastReceiver.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.app.Service
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.IBinder
+import android.telephony.TelephonyManager
+
+class ServicePhoneStateWithBroadcastReceiver : Service() {
+    val br : MyBroadcastReceiver = MyBroadcastReceiver()
+    val iFilter : IntentFilter = IntentFilter(TelephonyManager.ACTION_PHONE_STATE_CHANGED)
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        registerReceiver(br, iFilter )
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        return Service.START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(br)
+    }
+
+}
